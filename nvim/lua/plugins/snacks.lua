de903a477525ec82627bd3419920a515eaf55738
Jonny Furness <jlsfurness@gmail.com>
2025-10-27 20:06:32 +0000

    disabled smooth scroll by default

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,17 +1,6 @@
 return {
-  "folke/snacks.nvim",
-  ---@type snacks.Config
+  "snacks.nvim",
   opts = {
-    explorer = {
-      -- your explorer configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-      win = {
-        number = true,
-        wo = {
-          smoothscroll = false,
-        },
-      },
-    },
+    scroll = { enabled = false },
   },
 }
